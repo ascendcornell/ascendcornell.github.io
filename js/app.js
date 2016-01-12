60affbace11bf4d62d8055b489497940e4797daa
Austin Liu <arl96@cornell.edu>
2016-01-12 18:09:43 -0500

    adding all needed pages

--- js/app.js
+++ js/app.js
@@ -6,16 +6,54 @@ app.config(function($routeProvider){
       templateUrl: "templates/main.html",
       controller: "main.ctrl"
     })
+
     .when("/about",
     {
       templateUrl: "templates/about.html",
       controller: "about.ctrl"
     })
+      .when("/about/professional",
+      {
+        templateUrl: "templates/about/professional.html",
+        controller: "about.ctrl"
+      })
+      .when("/about/marketing",
+      {
+        templateUrl: "templates/about/marketing.html",
+        controller: "about.ctrl"
+      })
+      .when("/about/internal",
+      {
+        templateUrl: "templates/about/internal.html",
+        controller: "about.ctrl"
+      })
+      .when("/about/career",
+      {
+        templateUrl: "templates/about/career.html",
+        controller: "about.ctrl"
+      })
+
     .when("/events",
     {
       templateUrl: "templates/events.html",
       controller: "events.ctrl"
     })
+      .when("/events/senior",
+      {
+        templateUrl: "templates/events/senior.html",
+        controller: "events.ctrl"
+      })    
+      .when("/events/mentor",
+      {
+        templateUrl: "templates/events/mentor.html",
+        controller: "events.ctrl"
+      })
+      .when("/events/national",
+      {
+        templateUrl: "templates/events/national.html",
+        controller: "events.ctrl"
+      })
+
     .when("/join",
     {
       templateUrl: "templates/join.html",
